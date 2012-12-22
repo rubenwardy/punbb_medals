7c2d9b87e3fe93fb42647bc52f44e8bd36f8f437
Rubenwardy <anjayward@gmail.com>
2012-12-22 16:41:54 +0000

    better code

--- display_medals.php
+++ display_medals.php
@@ -1,6 +1,6 @@
 <?php
 
-require "medal_types.php";
+require_once "medal_types.php";
 
 echo "<li style=\"margin-top:10px;\">\n";
 
@@ -9,12 +9,21 @@ $medals_i=$cur_post['medals'];
 $medals=explode(",",$medals_i);
 
 while (list($key,$val)=each($medals)){
-  $med_img="q";
+  if ($val!=""){
+  $med_img="medal_q.png";
+  $med_titl=$val;
 
-  if (isset($medal_types[strtolower($val)]))
-     $med_img=$medal_types[strtolower($val)];
+  if (isset($medal_types[strtolower($val)]['name']))
+     $med_titl=$medal_types[strtolower($val)]['name'];
 
- echo "<img src=\"img/medal_$med_img.png\" title=\"$val\">\n";
+  if (isset($medal_types[strtolower($val)]['image']))
+     $med_img=$medal_types[strtolower($val)]['image'];
+
+  if (isset($medal_types[strtolower($val)]['description']))
+     $med_titl.=" - ".$medal_types[strtolower($val)]['description'];
+
+ echo "<img src=\"img/$med_img\" title=\"$med_titl\">\n";
+  }
 }
 
 echo "</li>\n";
--- medal_types.php
+++ medal_types.php
@@ -1,35 +1,77 @@
 <?php
 // WHAT DOES THIS MEAN?
-// [medal name] => [icon_name]
-// An icon name of "foo" would made the medal icon be: img/medal_foo.png
+// [medal id] => [name of medal], [icon of medal], [description]
+// An icon name of "medal_foo.png" would make the medal icon be: img/medal_foo.png
 
   $medal_types = array(
-  
-        // How many mods have they made?
-  	"first mod" => "bronze", // has made a mod
-  	"many mods" => "silver", // has made over 5 mods
-  	"mod factory" => "gold", // has made over 10 mods
 
-        // How creative are their mods?
-  	"mod pioneer" => "silver", // has made a mod considered innovative
-  	"master mod pioneer" => "gold", //has made 7 or more mods considered innovative
-  
-        // How many servers do they have?
-  	"first server" => "bronze", // owns a server
-  	"server owner" => "gold", // owns 2 or more servers
-  	"master server owner" => "gold", // owns 3 or more servers
 
-        // How popular is their server?
-  	"server owner" => "bronze", // owns an active server
-  	"big server owner" => "silver", // owns a server with over a 500 replies
-  	"mega server owner" => "gold", // owns a server with over a 1000 replies
+        // Mods
+  	"first mod" => array(
+  	       "name" => "First Mod",
+               "image" => "medal_bronze.png",
+               "description" => "has made a mod",
+        ),
+        "mod pioneer" => array(
+  	       "name" => "Mod Pioneer",
+               "image" => "medal_inno.png",
+               "description" => "an innovative mod maker: thinks outside of the box",
+        ),
+        "modf" => array(
+  	       "name" => "Mod Factory",
+               "image" => "medal_modf.png",
+               "description" => "has made 10 or more mods",
+        ),
+        
 
-        // Have they won a competition?
-  	"competition winner" => "gold", // won a competition
 
-        // How much do they contribute?
-  	"contributer" => "gold", // someone who has contributed a lot to the development, whether that be c++, mods or other
-  	"developer" => "silver", // someone who has done some c++ contribution
-  	"master developer" => "gold", // someone who has done a lot of c++ contribution
+
+        // Servers
+        "server owner" => array(
+  	       "name" => "Server Owner",
+               "image" => "medal_bronze.png",
+               "description" => "owns an active server",
+        ),
+        "popular host" => array(
+  	       "name" => "Popular Host",
+               "image" => "medal_silver.png",
+               "description" => "owns a server with over 500 replies",
+        ),
+        
+        
+
+
+
+        // Competition
+        "comp_2" => array(
+  	       "name" => "Competition Runner Up",
+               "image" => "medal_comp_2.png",
+               "description" => "came second in a competition",
+        ),
+        "comp_1" => array(
+  	       "name" => "Competition Winner",
+               "image" => "medal_comp.png",
+               "description" => "came first in a competition",
+        ),
+        
+        
+        
+
+
+        // Contribution and Development
+        "rainbow award" => array(
+  	       "name" => "Rainbow Award",
+               "image" => "medal_rainbow.png",
+               "description" => "helps other players with their problems",
+        ),
+        
+        "nyan cat award" => array(
+  	       "name" => "Nyan Cat Award",
+               "image" => "medal_nyan.png",
+               "description" => "has done something that helps the community move forward",
+        ),
+        
+        
+
   );
 ?>
